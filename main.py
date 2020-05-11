0ae98ed6bceb0c8a83f115dec8cb3e9c9e5ae9dd
Niels Neerhoff <nielsneerhoff@gmail.com>
2020-05-11 11:50:30 +0200

    Simplified value iteration.

--- main.py
+++ main.py
@@ -11,21 +11,19 @@ def value_iteration(env, max_iterations = 100000, gamma = 0.9, delta = 1e-04):
         for state in range(env.nS):
             action_values = np.zeros(env.nA)
             for action in range(env.nA):
-                state_value = 0
+                action_values[action] = 0
                 for j in range(len(env.P[state][action])):
                     prob, next_state, reward, done = env.P[state][action][j]
-                    state_action_value = prob * (reward + gamma * Qold[next_state])
-                    state_value += state_action_value
-                action_values[action] = state_value
+                    action_values[action] += prob * (reward + gamma * Qold[next_state])
             best_action = np.argmax(action_values)
             Qnew[state] = action_values[best_action]
         if i > 1000 and abs(np.sum(Qold) - np.sum(Qnew)) < delta:
             break
         else:
             Qold = Qnew.copy()
-    return max_policy(env, Qnew)
+    return max_policy(env, Qnew, gamma)
 
-def max_policy(env, Q, gamma = 0.9):
+def max_policy(env, Q, gamma):
     best_actions = np.zeros(env.nS)
     for state in range(env.nS):
         action_values = np.zeros(env.nA)
