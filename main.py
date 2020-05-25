b72ec503e2b8c68f5b74addcd0c8e582bfd9401d
Niels Neerhoff <nielsneerhoff@gmail.com>
2020-05-25 16:10:29 +0200

    Added working version of expert model BMDP.

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import gym
 import numpy as np
 
 from agent import MBIE, MBIE_EB
+from expert import BoundedParameterExpert
+from pseudo_env import PseudoEnv
 
 env = gym.make("gym_factored:river-swim-v0")
 
 def learn_online(
-    env, agent, max_episodes = 10000, gamma = 0.95, max_iterations = 1000, delta = 1e-02):
+    env, agent, expert, max_episodes = 10000, gamma = 0.95, max_iterations = 1000, delta = 1e-02):
     state = env.reset()
+    # Compute the best policy based on expert model.
+    expert.value_iteration(max_iterations, delta)
     for i in range(max_episodes):
-        action = agent.select_action(state)
+        action = expert.select_action(state)
         new_state, reward, done, info = env.step(action)
         agent.process_experience(state, action, new_state, reward, done)
         if i % 100 == 0:
@@ -20,7 +24,9 @@ def learn_online(
 
 agent = MBIE(env, 0.95, 10, 0.5, 0.5)
 # agent = MBIE_EB(env, 40000, 0.95)
-print(learn_online(env, agent))
+pseudo_env = PseudoEnv(env, 0.2)
+expert = BoundedParameterExpert(pseudo_env, 0.95)
+print(learn_online(env, agent, expert))
 
 def value_iteration(env, max_iterations = 10000, gamma = 0.9, delta = 1e-04):
     Qold = np.zeros(env.nS)
