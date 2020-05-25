0f2579bfc3f8982ff7264a163a763644ecafe567
Niels Neerhoff <nielsneerhoff@gmail.com>
2020-05-25 17:29:12 +0200

    Simplified main.

--- main.py
+++ main.py
@@ -26,36 +26,3 @@ agent = MBIE(env, 0.95, 10, 0.5, 0.5)
 pseudo_env = PseudoEnv(env, 0.2)
 expert = BoundedParameterExpert(pseudo_env, 0.95)
 print(learn_online(env, agent, expert))
-
-def value_iteration(env, max_iterations = 10000, gamma = 0.9, delta = 1e-04):
-    Qold = np.zeros(env.nS)
-    Qnew = Qold.copy()
-    for i in range(max_iterations):
-        for state in range(env.nS):
-            action_values = np.zeros(env.nA)
-            for action in range(env.nA):
-                action_values[action] = 0
-                for j in range(len(env.P[state][action])):
-                    prob, next_state, reward, done = env.P[state][action][j]
-                    action_values[action] += prob * (reward + gamma * Qold[next_state])
-            best_action = np.argmax(action_values)
-            Qnew[state] = action_values[best_action]
-        if i > 1000 and abs(np.sum(Qold) - np.sum(Qnew)) < delta:
-            break
-        else:
-            Qold = Qnew.copy()
-    return Qnew, max_policy(env, Qnew, gamma)
-
-def max_policy(env, Q, gamma):
-    best_actions = np.zeros(env.nS)
-    for state in range(env.nS):
-        action_values = np.zeros(env.nA)
-        for action in range(env.nA):
-            state_value = 0
-            for j in range(len(env.P[state][action])):
-                prob, next_state, reward, done = env.P[state][action][j]
-                state_action_value = prob * (reward + gamma * Q[next_state])
-                state_value += state_action_value
-            action_values[action] = state_value
-        best_actions[state] = np.argmax(action_values)
-    return best_actions
