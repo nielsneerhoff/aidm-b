132ebd83533fa062983ac62332716e2b31e60bef
Maarten van 't Wout <m.a.vantwout@student.tudelft.nl>
2020-05-27 17:02:26 +0200

    started coverage error and added cum rewards

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def learn_online(
         agent.process_experience(state, action, new_state, reward, done)
         metrics.increase_cum_reward(reward)
         agent.value_iteration(max_iterations, delta)
-        print('Iteration', i, '\t', agent.Q, '\t', metrics.get_cum_reward())
+        # print('Iteration', i, '\t', agent.Q, '\t', metrics.get_cum_reward()
+        print(agent.max_policy)
         state = new_state
 
     return agent.Q, metrics.get_cum_reward()
@@ -39,8 +40,9 @@ metrics = MetricsTracker(agent)
 
 
 agent = MBIE(env, 0.95, 10, 0.5, 0.5)
+
 # agent = MBIE_EB(env, 40000, 0.95)
-pseudo_env = PseudoEnv(env, 0.2)
+# pseudo_env = PseudoEnv(env, 0.2)
 # expert = BoundedParameterExpert(pseudo_env, 0.95)
 # print(learn_online(env, agent, expert))
 print(learn_online(env, metrics, agent))
--- metrics.py
+++ metrics.py
@@ -4,9 +4,55 @@ class MetricsTracker:
         self.agent = agent
         self.cum_reward = 0
     
+    #Cumulative Reward
     def increase_cum_reward(self, reward):
         self.cum_reward += reward
 
     def get_cum_reward(self):
         return self.cum_reward
+
+    #Coverage Error
+    
+
+
+def optimal_vi(env, max_iterations = 2, gamma = 0.95, delta = 1e-02):
+    Qold = np.zeros(env.nS)
+    Qnew = Qold.copy()
+    for i in range(max_iterations):
+        for state in range(env.nS):
+            action_values = np.zeros(env.nA)
+            for action in range(env.nA):
+                action_values[action] = 0
+                for j in range(len(env.P[state][action])):
+                    prob, next_state, reward, done = env.P[state][action][j]
+                    action_values[action] += prob * (reward + gamma * Qold[next_state])
+            best_action = np.argmax(action_values)
+            Qnew[state] = action_values[best_action]
+        if i > 1000 and abs(np.sum(Qold) - np.sum(Qnew)) < delta:
+            break
+        else:
+            Qold = Qnew.copy()
+    return optimal_max_policy(env, Qnew, gamma)
+
+def optimal_max_policy(env, Q, gamma):
+    best_actions = np.zeros(env.nS)
+    for state in range(env.nS):
+        action_values = np.zeros(env.nA)
+        for action in range(env.nA):
+            state_value = 0
+            for j in range(len(env.P[state][action])):
+                prob, next_state, reward, done = env.P[state][action][j]
+                state_action_value = prob * (reward + gamma * Q[next_state])
+                state_value += state_action_value
+            action_values[action] = state_value
+        best_actions[state] = np.argmax(action_values)
+    return best_actions
+
+
+print(optimal_vi(env))
+
+
+
+    
+
 
