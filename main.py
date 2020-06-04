9a3c30125566dc2cad70ba7b1e0e53e1ce22dc49
Berend-Jan Lange <lange.berendjan@gmail.com>
2020-06-04 12:26:05 +0200

    small update metrics writing main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from metrics import Metrics, write_metrics_to_file
 
 env = gym.make("gym_factored:river-swim-v0")
 
-def learn_online(env, agent, mediator):
+def learn_online(env, agent, mediator, metrics):
     state = env.reset()
     cum_reward = 0
     agent_model = None
@@ -21,7 +21,7 @@ def learn_online(env, agent, mediator):
         # print(state, action, new_state)
         cum_reward += reward
         agent.process_experience(state, action, new_state, reward, done)
-        # metrics_eb.update_metrics(state, action, reward, i)
+        metrics.update_metrics(state, action, reward, i)
         state = new_state
         if i % 100 == 0:
             agent.value_iteration(MAX_ITERATIONS, DELTA)
@@ -30,7 +30,7 @@ def learn_online(env, agent, mediator):
             # if agent_model.T_high[0, 1, 3] - agent_model.T_low[0, 1, 3] < 0.3:
             #     action = mediator.select_action(state, agent_model)
                 # mediator_action = mediator.select_action(state, agent_model)
-                # print(metrics_eb)
+                # print(metrics)
     return agent.Q, cum_reward
 
 # Initialize agents.
@@ -53,8 +53,9 @@ expert = BoundedParameterExpert(expert_model)
 mediator = Mediator(expert)
 
 # Initialize metrics.
-# metrics_eb = Metrics(mbie_eb_agent, env)
+metrics_eb = Metrics(mbie_eb_agent, env, 'MBIE_EB')
+metrics = Metrics(mbie_agent, env, 'MBIE')
 
 # expert.value_iteration()
 print(learn_online(env, mbie_agent, mediator))
-# write_metrics_to_file([metrics_eb], 'OUTPUT', 'test_run')
+write_metrics_to_file([metrics_eb, metrics], 'OUTPUT', 'test_run')
