f233cdc1a89ab4f62226d00275abf94a1ab62a62
Maarten van 't Wout <m.a.vantwout@student.tudelft.nl>
2020-06-12 16:29:30 +0200

    added the Simple Taxi environment

--- gym-factored/gym_factored/envs/simpletaxi.py
+++ gym-factored/gym_factored/envs/simpletaxi.py
@@ -1,22 +1,19 @@
 import numpy as np
 from gym.envs.toy_text.discrete import DiscreteEnv
 
-ACTION0 = 0
 ACTION1 = 1
+ACTION2 = 2
+ACTION3 = 3
 
-
-class OptPes(DiscreteEnv):
+class SimpleTaxi(DiscreteEnv):
     """
-    this is a OptPes env using the DiscreteEnv class
-
-    for a more general implementation of a chain env checkout gym/envs/toy_text/nchain.py
+    this is a SimpleTaxi env using the DiscreteEnv class
 
     """
-
     def __init__(self):
-        self.reward_range = 0, 3200
-        ns = 5
-        na = 2 
+        self.reward_range = 0, 100
+        ns = 4
+        na = 3 
         terminal_states = np.zeros(ns, dtype=bool)
 
         t = self.get_transition_function(na, ns)
@@ -34,36 +31,33 @@ class OptPes(DiscreteEnv):
                     p[s][a].append((transition_prob, new_state, reward, done))
         isd /= isd.sum()
         DiscreteEnv.__init__(self, ns, na, p, isd)
-
     def get_transition_function(self, na, ns):
         t = np.zeros((ns, na, ns))
 
         #State 0
-        t[0, ACTION0, 1] = 0.7
-        t[0, ACTION0, 2] = 0.3
-        t[0, ACTION1, 3] = 0.7
-        t[0, ACTION1, 4] = 0.3
+        t[0, ACTION1, 1] = 0.75
+        t[0, ACTION1, 0] = 0.25
+        t[0, ACTION2, 2] = 0.70
+        t[0, ACTION2, 0] = 0.30
+        t[0, ACTION3, 3] = 0.8
+        t[0, ACTION3, 0] = 0.20
 
 
         #State 1
-        t[1, ACTION0, 1] = 0.9
-        t[1, ACTION0, 0] = 0.1
+        t[1, ACTION1, 0] = 1
+        t[1, ACTION2, 0] = 1
+        t[1, ACTION3, 0] = 1
        
    
         #State 2
-        t[2, ACTION0, 2] = 0.9
-        t[2, ACTION0, 0] = 0.1
-       
+        t[2, ACTION1, 0] = 1
+        t[2, ACTION2, 0] = 1
+        t[2, ACTION3, 0] = 1
 
         #State 3
-        t[3, ACTION0, 3] = 0.9
-        t[3, ACTION0, 0] = 0.1
-       
-
-        #State 4
-        t[4, ACTION0, 4] = 0.9
-        t[4, ACTION0, 0] = 0.1
-        
+        t[3, ACTION1, 0] = 1
+        t[3, ACTION2, 0] = 1
+        t[3, ACTION3, 0] = 1
 
         return t
 
@@ -71,17 +65,12 @@ class OptPes(DiscreteEnv):
         r = np.zeros((ns, na, ns))
 
 
-        #State 1
-        r[1, ACTION0, 1] = 3100
-        
-        #State 2
-        r[2, ACTION0, 2] = 2000
-        
-        #State 3
-        r[3, ACTION0, 3] = 3200
-
-        #State 4
-        r[4, ACTION0, 4] = 0
+        t[0, ACTION1, 1] = 100
+        t[0, ACTION1, 0] = 0
+        t[0, ACTION2, 2] = 100
+        t[0, ACTION2, 0] = 0
+        t[0, ACTION3, 3] = 100
+        t[0, ACTION3, 0] = 0
      
         return r
 
