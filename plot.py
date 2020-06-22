fa5d9bebbad01d12e529b0ad1bc2cbfd07d46e7f
Niels Neerhoff <nielsneerhoff@gmail.com>
2020-06-22 13:19:28 +0200

    Added plots.

--- plot.py
+++ plot.py
@@ -4,8 +4,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-sns.set()
-
 DATA_ROOT = 'river-swim/'
 
 NUM_RESULTS = 250
@@ -19,22 +17,26 @@ for file in baseline_files:
     # File is mbie-eb.
     if file.startswith('river-swim/mbie-eb'):
         metric = file.replace('river-swim/mbie-eb_', '').replace('.dat', '')
-        if metric != 'sample_complexity':
+        if metric not in (
+            'sample_complexity','coverage_error_squared_R', 'reward_timeline', 'state_action_count'):
             data = pd.read_csv(file, sep = r'\t+', engine = 'python')
             columns = np.array(data.columns)
             mbie_eb[metric] = {}
             mbie_eb[metric]['mean'] = np.array(data[columns[1]])
-            mbie_eb[metric]['std'] = np.array(data[columns[2]]) - mbie_eb[metric]['mean']
+            mbie_eb[metric]['std_up'] = np.array(data[columns[2]])
+            mbie_eb[metric]['std_down'] = np.array(data[columns[3]])
 
     # File is mbie.
     else:
         metric = file.replace('river-swim/mbie_', '').replace('.dat', '')
-        if metric != 'sample_complexity':
+        if metric not in (
+            'sample_complexity','coverage_error_squared_R', 'reward_timeline', 'state_action_count'):
             data = pd.read_csv(file, sep = r'\t+', engine = 'python')
             columns = np.array(data.columns)
             mbie[metric] = {}
-            mbie[metric]['mean'] = np.array(data[columns[1]])
-            mbie[metric]['std'] = np.array(data[columns[2]]) - mbie[metric]['mean']
+            mbie[metric]['mean'] = np.array(data[columns[1]], dtype=float)
+            mbie[metric]['std_up'] = np.array(data[columns[2]], dtype=float)
+            mbie[metric]['std_down'] = np.array(data[columns[3]], dtype=float)
 
 ######################################################
 
@@ -50,7 +52,7 @@ for file in mediator_max_opt_files:
     rho = float(info[1].split('_')[0])
     metric = info[1].replace(f'{str(rho)}_', '').replace('.dat', '')
     if metric not in (
-        'sample_complexity','coverage_error_squared_R', 'reward_timeline'):
+        'sample_complexity','coverage_error_squared_R', 'reward_timeline', 'state_action_count'):
         data = pd.read_csv(file, sep = r'\t+', engine = 'python')
         columns = np.array(data.columns)
 
@@ -63,9 +65,11 @@ for file in mediator_max_opt_files:
             mediator_max_opt[metric][offset][rho] = {}
 
         mediator_max_opt[metric][offset][rho]['mean'] = \
-            np.array(data[columns[1]])
-        mediator_max_opt[metric][offset][rho]['std'] \
-            = np.array(data[columns[2]]) - np.array(data[columns[1]])
+            np.array(data[columns[1]], dtype=float)
+        mediator_max_opt[metric][offset][rho]['std_up'] \
+            = np.array(data[columns[2]], dtype=float)
+        mediator_max_opt[metric][offset][rho]['std_down'] \
+            = np.array(data[columns[3]], dtype=float)
 
 ######################################################
 
@@ -95,19 +99,24 @@ for file in mediator_random_files:
 
         mediator_random[metric][offset][rho]['mean'] = \
             np.array(data[columns[1]])
-        mediator_random[metric][offset][rho]['std'] \
-            = np.array(data[columns[2]]) - np.array(data[columns[1]])
+        mediator_random[metric][offset][rho]['std_up'] \
+            = np.array(data[columns[2]])
+        mediator_random[metric][offset][rho]['std_down'] \
+            = np.array(data[columns[3]])
 
 ######################################################
 
 # metric = 'cumulative_instantaneous_loss' # CHANGE FOR PLOT.
 metric = 'cumulative_rewards' # CHANGE FOR PLOT.
+# metric = 'coverage_error_squared_T' # CHANGE FOR PLOT.
 
 offsets = [0, 0.1, 0.2, 0.3, 0.5, 1.0]
 rhos = [0.02, 0.04, 0.08, 0.16, 0.32]
 
 NUM_ROWS = 6 # offset
 NUM_COLS = 5 # rho
+
+plt.rcParams.update({'font.size': 7})
 
 fig, axs = plt.subplots(
     NUM_ROWS, NUM_COLS, sharex=True, sharey=True)
@@ -120,10 +129,38 @@ for r in range(NUM_ROWS):
         mbie_data = mbie[metric]['mean'][:NUM_RESULTS]
         mbie_eb_data = mbie_eb[metric]['mean'][:NUM_RESULTS]
         index = range(NUM_RESULTS)
-        sns.lineplot(x = index, y = max_opt, ax = axs[r, c], color = 'red', alpha = 0.5)
-        sns.lineplot(x = index, y = random, ax = axs[r, c], color = 'orange', alpha = 0.5)
-        sns.lineplot(x = index, y = mbie_data, ax = axs[r, c], color = 'blue', alpha = 0.5)
-        sns.lineplot(x = index, y = mbie_eb_data, ax = axs[r, c], color = 'cyan', alpha = 0.5)
-        axs[r, c].set_title(f'({offset}, {rho})')
-        # axs[r, c].set_yscale('log')
+
+        max_opt_std_up = mediator_max_opt[metric][offset][rho]['std_up'][:NUM_RESULTS]
+        max_opt_std_down = mediator_max_opt[metric][offset][rho]['std_down'][:NUM_RESULTS]
+        random_std_up = mediator_random[metric][offset][rho]['std_up'][:NUM_RESULTS]
+        random_std_down = mediator_random[metric][offset][rho]['std_down'][:NUM_RESULTS]
+        mbie_data_std_up = mbie[metric]['std_up'][:NUM_RESULTS]
+        mbie_data_std_down = mbie[metric]['std_down'][:NUM_RESULTS]
+        mbie_eb_data_std_up = mbie_eb[metric]['std_up'][:NUM_RESULTS]
+        mbie_eb_data_std_down = mbie_eb[metric]['std_down'][:NUM_RESULTS]
+
+        # Plot the sds.
+        axs[r, c].fill_between(
+            index, max_opt_std_down, max_opt_std_up, color = 'red', alpha = 0.1)
+        axs[r, c].fill_between(
+            index, random_std_down, random_std_up, color = 'orange', alpha = 0.1)
+        axs[r, c].fill_between(
+            index, mbie_data_std_down, mbie_data_std_up, color = 'blue', alpha = 0.1)
+        axs[r, c].fill_between(
+            index, mbie_eb_data_std_down, mbie_eb_data_std_up, color = 'cyan', alpha = 0.1)
+
+        # Plot the means.
+        sns.lineplot(x = index, y = max_opt, ax = axs[r, c], color = 'red', alpha = 0.6)
+        sns.lineplot(x = index, y = random, ax = axs[r, c], color = 'orange', alpha = 0.6)
+        sns.lineplot(x = index, y = mbie_data, ax = axs[r, c], color = 'blue', alpha = 0.6)
+        sns.lineplot(x = index, y = mbie_eb_data, ax = axs[r, c], color = 'cyan', alpha = 0.6)
+        axs[r, c].set_title(f'({offset}, {rho})', fontsize = 9)
+
+        handles, labels = axs[r, c].get_legend_handles_labels()
+fig.legend(handles, labels, loc='upper center')
+
+plt.ticklabel_format(style='scientific', axis='y', scilimits=(0, 3))
+fig.tight_layout(pad=0.003)
+fig.text(0.5, 0.005, 'Number of steps', ha='center', fontsize = 9)
+fig.text(0.005, 0.5, 'Cumulative reward', va='center', rotation='vertical', fontsize = 9)
 plt.show()
