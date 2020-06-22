ccc5d1dd718970ce616edb45cc0f03fba29e56f3
vantwoutmaarten <m.a.vantwout@student.tudelft.nl>
2020-06-22 23:42:20 +0200

    add 0.3 and 0.5 max opt and random mediator

--- experiment.py
+++ experiment.py
@@ -33,7 +33,9 @@ R = expected_rewards(env) # Assume we have perfect knowledge of R.
 
 ####################### Mediator ########################
 # offsets = [0, 0.1, 0.2] # Maarten
-offsets = [0.3, 0.5, 1] # Niels
+# offsets = [0.3, 0.5, 1] # Niels
+offsets = [0.3, 0.5, 1]
+
 for offset in offsets:
     expert_model = OffsetModel.from_env(env, offset)
     rhos = [0.02, 0.04, 0.08, 0.16, 0.32]
@@ -42,14 +44,14 @@ for offset in offsets:
         print(offset, rho)
 
         # # Max-opt mediator.
-        # mediator_max_opt = Mediator(
-        #     expert_model, rho, safe_action_mode = 'max-opt')
-        # metrics = Metrics(
-        #     mediator_max_opt, env, 'mediator-max-opt')
-        # learn_online(env, mediator_max_opt, metrics)
-        # write_metrics_to_file(
-        #     [metrics], 'simple-taxi/mediator-max-opt',
-        #     prefix = f'mediator-max-opt-{offset}-{rho}')
+        mediator_max_opt = Mediator(
+            expert_model, rho, safe_action_mode = 'max-opt')
+        metrics = Metrics(
+            mediator_max_opt, env, 'mediator-max-opt')
+        learn_online(env, mediator_max_opt, metrics)
+        write_metrics_to_file(
+            [metrics], 'simple-taxi/mediator-max-opt',
+            prefix = f'mediator-max-opt-{offset}-{rho}')
 
         # Random mediator.
         mediator_random = Mediator(
